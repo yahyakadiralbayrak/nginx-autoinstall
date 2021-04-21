7c6441914e665f858d93d2667f6ed80c02e88c83
Yahya Kadir ALBAYRAK <ykalbayrak53@gmail.com>
2021-04-21 23:22:12 +0300

    Update nginx-autoinstall.sh

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -344,7 +344,7 @@ case $OPTION in
 	if [[ ! -e /etc/nginx/nginx.conf ]]; then
 		mkdir -p /etc/nginx
 		cd /etc/nginx || exit 1
-		wget https://raw.githubusercontent.com/Angristan/nginx-autoinstall/master/conf/nginx.conf
+		wget https://raw.githubusercontent.com/yahyakadiralbayrak/nginx-autoinstall/master/conf/nginx.conf
 	fi
 	cd /usr/local/src/nginx/nginx-${NGINX_VER} || exit 1
 
@@ -538,7 +538,7 @@ case $OPTION in
 
 		else
 			# Same patch as above but fixed conflicts with the HTTP/3 patch
-			wget https://raw.githubusercontent.com/angristan/nginx-autoinstall/master/patches/nginx_hpack_push_with_http3.patch -O nginx_http2_hpack.patch
+			wget https://raw.githubusercontent.com/yahyakadiralbayrak/nginx-autoinstall/master/patches/nginx_hpack_push_with_http3.patch -O nginx_http2_hpack.patch
 		fi
 		patch -p1 <nginx_http2_hpack.patch
 
@@ -564,14 +564,14 @@ case $OPTION in
 	# Using the official systemd script and logrotate conf from nginx.org
 	if [[ ! -e /lib/systemd/system/nginx.service ]]; then
 		cd /lib/systemd/system/ || exit 1
-		wget https://raw.githubusercontent.com/Angristan/nginx-autoinstall/master/conf/nginx.service
+		wget https://raw.githubusercontent.com/yahyakadiralbayrak/nginx-autoinstall/master/conf/nginx.service
 		# Enable nginx start at boot
 		systemctl enable nginx
 	fi
 
 	if [[ ! -e /etc/logrotate.d/nginx ]]; then
 		cd /etc/logrotate.d/ || exit 1
-		wget https://raw.githubusercontent.com/Angristan/nginx-autoinstall/master/conf/nginx-logrotate -O nginx
+		wget https://raw.githubusercontent.com/yahyakadiralbayrak/nginx-autoinstall/master/conf/nginx-logrotate -O nginx
 	fi
 
 	# Nginx's cache directory is not created by default
@@ -652,7 +652,7 @@ case $OPTION in
 	exit
 	;;
 3) # Update the script
-	wget https://raw.githubusercontent.com/Angristan/nginx-autoinstall/master/nginx-autoinstall.sh -O nginx-autoinstall.sh
+	wget https://raw.githubusercontent.com/yahyakadiralbayrak/nginx-autoinstall/master/nginx-autoinstall.sh -O nginx-autoinstall.sh
 	chmod +x nginx-autoinstall.sh
 	echo ""
 	echo "Update done."
